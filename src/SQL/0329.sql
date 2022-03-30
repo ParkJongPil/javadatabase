32b8c8114120fb37ac68820bf6c1510522a0e6d0
Park Jong Pil <pjp8191@naver.com>
2022-03-30 09:41:51 +0900

    0330 DatabaseProject

---
+++ src/SQL/0329.sql
@@ -0,0 +1,21 @@
+show tables;
+
+desc aaa;
+
+drop table aaa;
+create table aaa (
+	name varchar(20) not null,
+	age int default 20,
+	gender char(2) default 'm',
+	joinday datetime default now()
+);
+	
+	insert into aaa values('홍길동',25,default, default);
+	insert into aaa values('홍길순',25,"f", default);
+	insert into aaa values('김말숙',35,"f", '2020-1-5');
+	insert into aaa values('이기자',35,"f", '2020-1-5');
+	
+	select * from aaa;
+	
+	
+	delete from aaa;
